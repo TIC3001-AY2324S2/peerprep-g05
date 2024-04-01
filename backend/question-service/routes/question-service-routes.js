e46f510ebead31077645256c6bdc08fe19e96550
gwee melvin <gwee_melvin@live.com>
2024-04-01 20:59:13 +0800

    fixed api route to call correct method

--- backend/question-service/routes/question-service-routes.js
+++ backend/question-service/routes/question-service-routes.js
@@ -1,8 +1,8 @@
 import express from "express";
 const router = express.Router();
 import { verifyAccessToken, verifyIsAdmin } from "../middleware/basic-access-control.js";
-import { getAllQuestion, getQuestionById, getOneQuestionByComplexity, createQuestion,
-    //getTotalQuestionCount,
+import { getAllQuestion, getQuestionById, getAllQuestionByComplexity, createQuestion,
+    //getTotalQuestionCount,getOneQuestionByComplexity,
      deleteQuestionById, updateQuestionById } from "../controller/question-controller.js";
 
 //return all question given active login + admin user
@@ -16,7 +16,7 @@ router.get('/api/question/:id', verifyAccessToken, getQuestionById);
 //router.get('/api/question/complexity/:complexity', verifyAccessToken, getOneQuestionByComplexity);
 
 //return all question with complexity
-router.get('/api/question/all/:complexity', verifyAccessToken, getAllQuestion);
+router.get('/api/question/all/:complexity', verifyAccessToken, getAllQuestionByComplexity);
 
 //create a single question and store in collection
 router.post('/api/question/create', verifyAccessToken, verifyIsAdmin, createQuestion);
